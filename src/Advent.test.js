eb18328cf471429d15aea2fdea972b095b21c4e9
Xing Zhou <xing@xingzhou.me>
2021-12-11 13:07:29 -0500

    feat: implement depth measurement increase logic

--- src/Advent.test.js
+++ src/Advent.test.js
@@ -1,7 +1,29 @@
+function depthMeasurementIncreases(measurements) {
+    const reducer = (previous, current, currentIndex, measurements) => {
+        if (current > measurements[currentIndex - 1]) {
+            return [...previous, 1];
+        }
+        return [...previous, 0]
+    };
+
+    const increases = measurements.reduce(reducer, []);
+    return increases.reduce((a, b) => a + b);
+}
+
 describe('Advent of Code 2021', () => {
     describe('day 1', () => {
-        it('counts the number of times a depth measurement increases', () => {
+        describe('counting the number of times a depth measurement increases', () => {
+            it('works for a single measurement', () => {
+                const measurements = [199];
+                const result = depthMeasurementIncreases(measurements);
+                expect(result).toBe(0);
+            })
 
+            it('works for multiple measurements', () => {
+                const measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+                const result = depthMeasurementIncreases(measurements);
+                expect(result).toBe(7);
+            })
         })
     })
 })
