44acb8ea9df8eda0cc385c64dc1b6ce1586fd214
Xing Zhou <xing@xingzhou.me>
2021-12-13 05:07:14 -0500

    refactor: remove duplication in creating file parsers

--- src/InputParser.js
+++ src/InputParser.js
@@ -2,139 +2,130 @@ const path = require('path')
 const fs = require('fs')
 const readline = require('readline')
 
-function day1Parser(rl) {
-    return (resolve) => {
-        let result = [];
+function createLineParser(readline, initialResult, handleLine) {
+    return new Promise((resolve) => {
+        let result = initialResult
 
-        function handleLine(line, result) {
-            if (line.length === 0) {
-                return result
-            }
-
-            return [...result, Number.parseInt(line)]
-        }
-
-        rl.on('line', (line) => {
+        readline.on('line', (line) => {
             result = handleLine(line, result);
         })
 
-        rl.on('close', () => {
+        readline.on('close', () => {
             resolve(result);
         })
-    }
+    });
 }
 
-function parseLinesIntoArray(rl) {
-    return (resolve) => {
-        let result = [];
+function day1Parser(readline) {
+    let initialResult = [];
 
-        function handleLine(line, result) {
-            if (line.length === 0) {
-                return result
-            }
-
-            return [...result, line]
+    function handleLine(line, result) {
+        if (line.length === 0) {
+            return result
         }
 
-        rl.on('line', (line) => {
-            result = handleLine(line, result);
-        })
-
-        rl.on('close', () => {
-            resolve(result);
-        })
+        return [...result, Number.parseInt(line)]
     }
+
+    return createLineParser(readline, initialResult, handleLine)
 }
 
-function day4Parser(rl) {
-    return (resolve) => {
+function parseLinesIntoArray(readline) {
+    let initialResult = [];
 
-        let result = {
-            boards: [],
-            numbersDrawn: undefined,
-        };
+    function handleLine(line, result) {
+        if (line.length === 0) {
+            return result
+        }
 
-        function handleLine(line, result) {
-            if (line.length === 0) {
-                return result;
+        return [...result, line]
+    }
+
+    return createLineParser(readline, initialResult, handleLine)
+}
+
+function day4Parser(readline) {
+    let initialResult = {
+        boards: [],
+        numbersDrawn: undefined,
+    };
+
+    function handleLine(line, result) {
+        if (line.length === 0) {
+            return result;
+        }
+
+        function handleBoardRow(line, result) {
+            function isTheBeginningOfANewBoard(boards) {
+                function lastBoardIsComplete() {
+                    return boards.at(-1).length === 5;
+                }
+
+                function hasNoBoards() {
+                    return boards.length === 0;
+                }
+
+                return hasNoBoards() || lastBoardIsComplete();
             }
 
-            function handleBoardRow(line, result) {
-                function isTheBeginningOfANewBoard(boards) {
-                    function lastBoardIsComplete() {
-                        return boards.at(-1).length === 5;
-                    }
+            const row = line.split(/\s+/).map(number => Number.parseInt(number));
+            const boards = result.boards;
 
-                    function hasNoBoards() {
-                        return boards.length === 0;
-                    }
-
-                    return hasNoBoards() || lastBoardIsComplete();
-                }
-
-                const row = line.split(/\s+/).map(number => Number.parseInt(number));
-                const boards = result.boards;
-
-                if (isTheBeginningOfANewBoard(boards)) {
-                    return {
-                        ...result,
-                        boards: [...boards, [row]]
-                    }
-                }
-
+            if (isTheBeginningOfANewBoard(boards)) {
                 return {
                     ...result,
-                    boards: [...(boards.slice(0, -1)), [...(boards.at(-1)), row]]
+                    boards: [...boards, [row]]
                 }
             }
 
-            function handleNumbersDrawn(line, result) {
-                return {
-                    ...result,
-                    numbersDrawn: line.split(',').map(number => Number.parseInt(number))
-                }
+            return {
+                ...result,
+                boards: [...(boards.slice(0, -1)), [...(boards.at(-1)), row]]
             }
-
-            if (result.numbersDrawn) {
-                return handleBoardRow(line, result);
-            }
-            return handleNumbersDrawn(line, result);
         }
 
-        rl.on('line', (line) => {
-            result = handleLine(line, result);
-        })
+        function handleNumbersDrawn(line, result) {
+            return {
+                ...result,
+                numbersDrawn: line.split(',').map(number => Number.parseInt(number))
+            }
+        }
 
-        rl.on('close', () => {
-            resolve(result);
-        })
+        if (result.numbersDrawn) {
+            return handleBoardRow(line, result);
+        }
+        return handleNumbersDrawn(line, result);
     }
+
+    return createLineParser(readline, initialResult, handleLine)
 }
 
-function getSpecificDayInputParser(fileName) {
+function rejectFile(fileName) {
+    return Promise.reject(`does not know how to handle ${fileName}`);
+}
+
+function createReadlineForFile(fileName) {
     const filePath = path.join(__dirname, 'inputs', `${fileName}.txt`)
-    const rl = readline.createInterface({
+    return readline.createInterface({
         input: fs.createReadStream(filePath),
         terminal: false
     });
-
-    switch (fileName) {
-        case 'day-1':
-            return day1Parser(rl)
-        case 'day-2':
-        case 'day-3':
-            return parseLinesIntoArray(rl);
-        case 'day-4':
-            return day4Parser(rl)
-        default:
-            return (_, reject) => reject(`does not know how to handle ${fileName}`)
-    }
-
 }
 
 function parse(fileName) {
-    const parseSpecificDayInput = getSpecificDayInputParser(fileName);
-    return new Promise(parseSpecificDayInput)
+    const readline = createReadlineForFile(fileName);
+
+    switch (fileName) {
+        case 'day-1':
+            return day1Parser(readline)
+        case 'day-2':
+        case 'day-3':
+            return parseLinesIntoArray(readline);
+        case 'day-4':
+            return day4Parser(readline)
+        default:
+            return rejectFile(fileName)
+    }
 }
 
 module.exports = parse;
