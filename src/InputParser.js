3dbcfb53cffbf0692b5d1de6ae4dff9a5bb96504
Xing Zhou <xing@xingzhou.me>
2021-12-13 02:43:18 -0500

    feat: parse bingo boards from input file

--- src/InputParser.js
+++ src/InputParser.js
@@ -10,14 +10,35 @@ function parse(fileName) {
     });
 
     return new Promise((resolve, reject) => {
-        const result = {};
+        const result = {
+            boards: [],
+            numbersDrawn: undefined,
+        };
+
+        function handleLine(line) {
+            if (line.length === 0) {
+                return;
+            }
+
+            if (result.numbersDrawn) {
+                const row = line.split(/\s+/).map(number => Number.parseInt(number));
+                const newBoard = [row];
+                if (result.boards.length === 0) {
+                    result.boards.push(newBoard)
+                } else {
+                    if (result.boards[result.boards.length - 1].length === 5) {
+                        result.boards.push(newBoard)
+                    } else {
+                        result.boards[result.boards.length - 1].push(row)
+                    }
+                }
+            } else {
+                result.numbersDrawn = line.split(',').map(number => Number.parseInt(number));
+            }
+        }
 
         rl.on('line', (line) => {
-            if (result.numbersDrawn) {
-
-            } else {
-                result.numbersDrawn = line.split(',');
-            }
+            handleLine(line);
         })
 
         rl.on('close', () => {
